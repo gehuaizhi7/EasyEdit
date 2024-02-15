c6c718f6854e0e17f6b0cc59abac0a73a53bd9c0
Huaizhi Ge <60053346+gehuaizhi7@users.noreply.github.com>
2024-02-15 12:16:16 -0500

    Update run_knowedit_llama2.py

--- examples/run_knowedit_llama2.py
+++ examples/run_knowedit_llama2.py
@@ -220,15 +220,11 @@ if __name__ == "__main__":
         os.makedirs(args.metrics_save_dir)
     # json.dump(metrics, open(os.path.join(args.metrics_save_dir, f'{args.editing_method}_results.json'), 'w'), indent=4)
 
-    file_path = os.path.join(args.metrics_save_dir, f'{args.editing_method}_results.json')
+    file_path = os.path.join(args.metrics_save_dir, f'{args.editing_method}_counterfact_results.json')
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
     with open(file_path, 'w') as f:
         json.dump(metrics, f, indent=4)
 
-    
-    json_open_file = open("test.json", "w")
-    json.dump(metrics, json_open_file, indent=4)
-    json_open_file.close()
         
-    print(metrics)
-    print("!!!!!")
+    # print(metrics)
+    # print("!!!!!")
