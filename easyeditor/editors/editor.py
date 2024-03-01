c02ba4c79448d00767e457c12b0369b8d7900744
Huaizhi Ge <60053346+gehuaizhi7@users.noreply.github.com>
2024-02-29 22:26:47 -0500

    Update editor.py

--- easyeditor/editors/editor.py
+++ easyeditor/editors/editor.py
@@ -372,10 +372,58 @@ class BaseEditor:
 
 
 
-              
 
-              
+
+
+                def test_batch_prediction(
+                    model,
+                    tok,
+                    prefixes: typing.List[str],
+                    target_new: str,
+                    target_true: str,
+                ):
+                    """ """
                 
+                    prefix_lens = [len(n) for n in tok(prefixes)["input_ids"]]
+                    prompt_tok = tok(
+                        [
+                            f"{prefix} {suffix}"
+                            for prefix in prefixes
+                            for suffix in [target_new, target_true]
+                        ],
+                        padding=True,
+                        return_tensors="pt",
+                    ).to("cuda")
+                
+                    a_tok, b_tok = (tok(f" {n}")["input_ids"] for n in [target_new, target_true])
+                    choice_a_len, choice_b_len = (len(n) for n in [a_tok, b_tok])
+                
+                    with torch.no_grad():
+                        logits = model(**prompt_tok).logits
+                
+                    results = np.zeros((logits.size(0),), dtype=np.float32)
+                
+                    for i in range(logits.size(0)):
+                        cur_len = choice_a_len if i % 2 == 0 else choice_b_len
+                        for j in range(cur_len):
+                            cur_tok = (a_tok if i % 2 == 0 else b_tok)[j]
+                            results[i] += -torch.nn.functional.log_softmax(
+                                logits[i, prefix_lens[i // 2] + j - 1, :], dim=0
+                            )[cur_tok].item()
+                        results[i] /= cur_len
+                
+                    return [
+                        {"target_new": results[i].item(), "target_true": results[i + 1].item()}
+                        for i in range(0, len(results), 2)
+                    ]
+
+
+                print("ccccccccccccccccccccc")
+                print(test_batch_prediction(edited_model, self.tok, list(prefixi), targeti, targeti))
+
+
+                
+      
 
                 start = time()
                 all_metrics[i].update({
